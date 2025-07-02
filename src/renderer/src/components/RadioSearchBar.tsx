55ecfd19efb0a4d3acaea941209ff52d0728cbf1
sarah.tsangou-wanvoukissa <sarah.tsangou-wanvoukissa@epita.fr>
2025-07-02 11:07:23 +0200

    feat(RadioSearchBar): radio is now updated when the 'enter' key is pressed

--- src/renderer/src/components/RadioSearchBar.tsx
+++ src/renderer/src/components/RadioSearchBar.tsx
@@ -1,3 +1,4 @@
+import { on } from 'events'
 import React from 'react'
 import { useState } from 'react'
 
@@ -17,19 +18,25 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   return (
     <div className="bg-transparent flex justify-center items-center">
       <div className=" rounded-t-xl w-2/3 h-8 bg-cyan-800 flex flex-row items-center justify-evenly px-4 py-1">
+        <form onSubmit={onSubmit}></form>
         <input
           type="text"
           className="rounded-full bg-cyan-100 h-lg w-3/4 text-xs px-2 text-gray-700 "
           placeholder="put your radio link here"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              onSubmit(e)
+            }
+          }}
         />
-        <button
+        <input
+          type="submit"
           className="bg-cyan-500 text-white px-2 py-1 rounded-full text-xs"
           onClick={onSubmit}
-        >
-          Jam!
-        </button>
+          value="Jam!"
+        />
       </div>
     </div>
   )
